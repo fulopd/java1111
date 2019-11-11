1830acc59ac67bd5763ed548c772237a94b59995
Fülöp Dániel <fulopdaniel@gmail.com>
2019-11-11 16:58:13 +0100

    osztály létrehozása

--- OOP/src/Ember.java
+++ OOP/src/Ember.java
@@ -1,4 +1,44 @@
 
 public class Ember {
 
+	// Alt + Shif + s - konstruktor / getter setter generálás
+	//Ctrl + shift + f - kód rendezés
+	private String vezetekNev;
+	private String keresztNev;
+	private int eletkor;
+
+	public Ember(String vezetekNev, String keresztNev, int eletkor) {
+		this.vezetekNev = vezetekNev;
+		this.keresztNev = keresztNev;
+		this.eletkor = eletkor;
+	}
+
+	public String getVezetekNev() {
+		return vezetekNev;
+	}
+
+	public void setVezetekNev(String vezetekNev) {
+		this.vezetekNev = vezetekNev;
+	}
+
+	public String getKeresztNev() {
+		return keresztNev;
+	}
+
+	public void setKeresztNev(String keresztNev) {
+		this.keresztNev = keresztNev;
+	}
+
+	public int getEletkor() {
+		return eletkor;
+	}
+
+	public void setEletkor(int eletkor) {
+		this.eletkor = eletkor;
+	}
+
+	@Override
+	public String toString() {
+		return vezetekNev + " " + keresztNev + " " + eletkor;
+	}
 }
--- OOP/src/Main.java
+++ OOP/src/Main.java
@@ -3,7 +3,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Ember e1 = new Ember("Kiss", "Béla", 11);
+		
+		e1.setEletkor(35);
+		
+		System.out.println(e1);
+		
+		
 	}
 
 }
